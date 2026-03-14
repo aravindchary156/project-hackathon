31192562da75d7cf51551d4864def604e12592a2
aravind <ias1.email@example.com>
2026-03-14 09:19:13 +0530

    Make kubernetes/helm providers safe before EKS exists (fallback values)

--- terraform code/provider.tf
+++ terraform code/provider.tf
@@ -30,17 +30,23 @@ provider "aws" {
   }
 }
 
+locals {
+  kube_host  = try(aws_eks_cluster.main.endpoint, "https://example.invalid")
+  kube_ca    = try(base64decode(aws_eks_cluster.main.certificate_authority[0].data), "")
+  kube_token = try(data.aws_eks_cluster_auth.main.token, "dummy")
+}
+
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+  host                   = local.kube_host
+  cluster_ca_certificate = local.kube_ca
+  token                  = local.kube_token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
+    host                   = local.kube_host
+    cluster_ca_certificate = local.kube_ca
+    token                  = local.kube_token
   }
 }
 
